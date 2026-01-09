2af9edcd3fb2b652fef20392d933f2d010f9ca78
Ryan <ryandias100@hotmail.com>
2026-01-09 01:00:51 -0300

    Configuração equals e hashcode

--- src/main/java/com/ryan/estudos_spring/model/Categoria.java
+++ src/main/java/com/ryan/estudos_spring/model/Categoria.java
@@ -3,6 +3,8 @@ package com.ryan.estudos_spring.model;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 
+import java.util.Objects;
+
 @Entity
 @Table(name = "Categorias")
 public class Categoria {
@@ -33,5 +35,26 @@ public class Categoria {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Categoria categoria = (Categoria) o;
+        return Objects.equals(id, categoria.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Categoria{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
     }
 }
