7489ca5bdafb81c47536f132cd08a4d29b8109ec
Ryan <ryandias100@hotmail.com>
2026-01-09 01:02:30 -0300

    Injeção de dependencias através de @Autowired

--- src/main/java/com/ryan/estudos_spring/controller/CategoriaController.java
+++ src/main/java/com/ryan/estudos_spring/controller/CategoriaController.java
@@ -1,33 +1,39 @@
 package com.ryan.estudos_spring.controller;
 
+import com.ryan.estudos_spring.exception.CategoriaNaoEncontradaException;
 import com.ryan.estudos_spring.model.Categoria;
 import com.ryan.estudos_spring.repository.CategoriaRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CategoriaController {
+    @Autowired
     private CategoriaRepository categoriaRepository;
 
-    CategoriaController(CategoriaRepository categoriaRepository){
-        this.categoriaRepository = categoriaRepository;
-    }
-
-    @GetMapping("/categoria")
+    @GetMapping("/categorias")
     public ResponseEntity<List<Categoria>> ListarCategorias(){
         return ResponseEntity.ok(categoriaRepository.findAll());
     }
 
-    @PostMapping("/categoria")
+    @GetMapping("/categorias/{id}")
+    public ResponseEntity<Categoria> buscarCategoria(@PathVariable Long id){
+        Optional<Categoria> categoria = categoriaRepository.findById(id);
+        if(categoria.isPresent()){
+            return  ResponseEntity.ok(categoria.get());
+        } throw new CategoriaNaoEncontradaException("Categoria id " + id + " não encontrado");
+    }
+
+    @PostMapping("/categorias")
     public ResponseEntity<Categoria> criarCategoria(@Valid @RequestBody Categoria categoria){
         if(categoria != null){
-            ResponseEntity.status(201).body(categoriaRepository.save(categoria));
+            return ResponseEntity.status(201).body(categoriaRepository.save(categoria));
         }return ResponseEntity.badRequest().build();
+
     }
 }
